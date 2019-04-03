955261e143a2659045ae1a861a76a8ebd148b927
mohitrajane <mhit98@gmail.com>
2019-04-03 12:52:51 +0530

    add function to send mail

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
+from email.mime.image import MIMEImage
 from email import encoders
 
 from PIL import Image, ImageDraw, ImageFont
@@ -28,6 +29,8 @@ def add_text(name_list, path):
         image.save(output_path, format='png')
         print("Saved:{}".format(output_path))
         image.close()
+        send_mail(name[1], output_path)
+        x = input("done with sending file")
 
 def parse_csv(path):
     name_list = list()
@@ -40,6 +43,44 @@ def parse_csv(path):
             flag = 1
     name_file.close()
     return name_list
+
+def send_mail(receiver_address, attach_file_name):
+    n = (attach_file_name.split('/'))[1].split('.')
+    print(n[0])
+    mail_content = 'Dear ' + n[0] +''',\nThank ouy for making Arduino Day 2019 conducted
+    by Fisat Students Developer Community(FSDC) a grand success. Your 
+    certificate has been attached to this mail
+    '''
+    sender_address = 'fisatsdc@gmail.com'
+    sender_pass = 'fisatsdc123#'
+    receiver_address = 'mhit98@gmail.com'
+
+    # Setup the MIME
+    message = MIMEMultipart()
+    message['From'] = sender_address
+    message['To'] = receiver_address
+    message['Subject'] = 'Arduino day certificate.'
+
+    # The body and the attachments for the mail
+    message.attach(MIMEText(mail_content, 'plain'))
+    attach_file = open(attach_file_name, 'rb').read()  # Open the file as binary mode
+    #payload = MIMEBase('application', 'octate-stream')
+    #payload.set_payload((attach_file).read())
+    #encoders.encode_base64(payload) #encode the attachment
+    # add payload header with filename
+    #payload.add_header('Content-Decomposition', 'attachment', filename=attach_file_name)
+    #message.attach(payload)
+    image = MIMEImage(attach_file, name=n[1])
+    message.attach(image)
+
+    # Create SMTP session for sending the mail
+    session = smtplib.SMTP('smtp.gmail.com', 587)  # use gmail with port
+    session.starttls()  # enable security
+    session.login(sender_address, sender_pass)  # login with mail_id and password
+    text = message.as_string()
+    session.sendmail(sender_address, receiver_address, text)
+    session.quit()
+    print('Mail Sent')
 
 if __name__ == '__main__':
     path = input("Enter image file:")
